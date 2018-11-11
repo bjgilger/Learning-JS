2b668b68feb26e58a980345085de5cf094de170f
Gilger, John <john.gilger@gmail.com>
2018-11-10 21:22:37 -0800

    Added Use IIFE...

---
+++ ObjectOrientedProgramming/UseIIFEtoCreate Module.js
@@ -0,0 +1,20 @@
+/**
+ * An immediately invoked function expression (IIFE) is often
+ * used to group related functionality into a single object
+ * or module.
+ */
+
+let funModule = (function() {
+ return {
+   isCuteMixin: function(obj) {
+     obj.isCute = function() {
+   return true;
+ };
+ },
+ singMixin: function(obj) {
+   obj.sing = function() {
+     console.log("Singing to an awesome tune");
+ };
+}
+}
+}) ();
